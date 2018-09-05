fde644a1647863a825c53ca8d7189148d2a48ccc
keithbaum <keith.baum@yahoo.com.ar>
2018-09-04 22:37:22 -0300

    Finished M calculation

--- calculate_M.py
+++ calculate_M.py
@@ -1,1 +1,7 @@
-#M=sigma+N*lambda*Eye
+import numpy as np
+from calculate_sigma import sigma
+
+def M(Lambda,Fi,f,F,p,tSpan):
+    Sigma = sigma(Fi, f, F, p, tSpan)
+    N=len(Fi)
+    return Sigma+N*Lambda*np.eye(Sigma.shape)
